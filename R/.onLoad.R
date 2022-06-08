dcc468275f5f22265cd49b0b7403ae9811de48c0
eirikmn <eirikmn91@gmail.com>
2022-06-08 15:01:56 +0200

    Delete .onLoad.R

--- R/.onLoad.R
+++
@@ -1,7 +0,0 @@
-if(!rlang::is_installed("INLA")){
-  warning(paste0("Package INLA not found. This can be installed by running: \n",
-                 "install.packages(\"INLA\",repos=c(getOption(\"repos\"),INLA=\"https://inla.r-inla-download.org/R/stable\"), dep=TRUE)",
-                 "\nfor the stable version or \n"),
-          "install.packages(\"INLA\",repos=c(getOption(\"repos\"),INLA=\"https://inla.r-inla-download.org/R/testing\"), dep=TRUE)",
-          "\nfor the testing version")
-}
